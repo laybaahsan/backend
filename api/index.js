147133c96f735efbd1d75487ef9732498252fc06
laybaahsan <laybaahsa5@gmail.com>
2025-08-18 10:37:11 -0700

    versal setup

---
+++ api/index.js
@@ -0,0 +1,101 @@
+const dotenv=require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+const cookieParser=require('cookie-parser');
+const session = require('express-session');
+
+
+//import DB connection
+const connectDB = require('./config/db');
+
+//import routes
+const userRoutes = require('./routes/user');
+const medicineRoutes = require('./routes/medicine');
+const historyRoutes = require('./routes/history');
+const forgetPasswordRoutes=require('./routes/forgetPassword');
+const adminRoutes = require('./routes/admin');
+const swaggerRoutes = require ('./routes/swagger');
+
+// Initialize express app
+const app = express();
+
+// ===========================Middleware================================//
+
+app.use(cors());
+app.use(express.static("public"));
+//JSON and URL parsing
+app.use(express.json( {
+  strict :true,
+  limit : '50mb',
+  verify :(req,res,buf)=>{
+    if (!buf.length){
+      req.body = {};//empty objects instead of throughing errors
+    }
+  }
+}
+));
+
+
+app.use(express.urlencoded({ extended: true , limit :'50mb' }));
+app.use(cookieParser());
+app.use(session({            //Session Management
+  secret: process.env.SESSION_SECRET|| 'session-secret', // Use env variable
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: process.env.NODE_ENV === 'production' } //  HTTPS for production
+}));
+
+
+// //Passport authentication
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+
+//================================ejs view engine==============================//
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+
+app.get('/', (req, res) => res.send('Welcome to MedScan!'));
+
+
+// Define User Routes
+app.use('/user', userRoutes);
+
+
+app.use('/medicine', medicineRoutes);
+app.use('/ocr', medicineRoutes);
+app.use('/barcode', medicineRoutes);
+app.use('/history', historyRoutes);
+app.use('/forgetPassword',forgetPasswordRoutes);
+app.use('/admin',adminRoutes);
+app.use('/api-docs',swaggerRoutes);
+
+
+connectDB();
+
+// --- sample route ---
+app.get('/api/health', (req, res) => {
+  res.json({ ok: true, message: 'Backend is live!' });
+});
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {console.log(`Server running on port ${PORT}`)
+
+// }
+// );
+
+
+ module.exports = app;
+
+
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -90,8 +90,7 @@ app.listen(PORT, () => {console.log(`Server running on port ${PORT}`)
 );
 
 
-
-// module.exports = app;
+ module.exports = app;
 
 
 
