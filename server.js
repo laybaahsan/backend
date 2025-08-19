ba8406f55977de1c6da12468e4a3749e43b34021
laybaahsan <laybaahsa5@gmail.com>
2025-08-18 23:13:23 -0700

    Update server.js and vercel.json for deployment

--- server.js
+++ server.js
@@ -98,11 +98,11 @@ const PORT = process.env.PORT || 3000;
 // }
 // );
 
-if(process.env.NODE_ENV !=='production');{
-  app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
-  })
-}
+// if(process.env.NODE_ENV !=='production');{
+//   app.listen(PORT,()=>{
+//     console.log(`Server is running on port ${PORT}`);
+//   })
+//}
 //export for vercel
  module.exports = app;
 
